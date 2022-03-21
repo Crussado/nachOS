bf93f97db7f33f9d4b28baf00a256ae3a8984a2b
Julio <julioolivencia891@gmail.com>
2022-03-20 22:19:30 -0300

    primer commit

---
+++ code/threads/thread_test_simple.cc
@@ -0,0 +1,52 @@
+/// Copyright (c) 1992-1993 The Regents of the University of California.
+///               2007-2009 Universidad de Las Palmas de Gran Canaria.
+///               2016-2021 Docentes de la Universidad Nacional de Rosario.
+/// All rights reserved.  See `copyright.h` for copyright notice and
+/// limitation of liability and disclaimer of warranty provisions.
+
+
+#include "thread_test_simple.hh"
+#include "system.hh"
+
+#include <stdio.h>
+#include <string.h>
+
+
+/// Loop 10 times, yielding the CPU to another ready thread each iteration.
+///
+/// * `name` points to a string with a thread name, just for debugging
+///   purposes.
+void
+SimpleThread(void *name_)
+{
+    // Reinterpret arg `name` as a string.
+    char *name = (char *) name_;
+
+    // If the lines dealing with interrupts are commented, the code will
+    // behave incorrectly, because printf execution may cause race
+    // conditions.
+    for (unsigned num = 0; num < 10; num++) {
+        printf("*** Thread `%s` is running: iteration %u\n", name, num);
+        currentThread->Yield();
+    }
+    printf("!!! Thread `%s` has finished\n", name);
+}
+
+/// Set up a ping-pong between several threads.
+///
+/// Do it by launching one thread which calls `SimpleThread`, and finally
+/// calling `SimpleThread` on the current thread.
+void
+ThreadTestSimple()
+{
+    unsigned cant_thread = 4;
+    const char *name[cant_thread] = {"2nd", "3rd", "4th", "5th"};
+    Thread *newThread[cant_thread];
+
+    for(unsigned num = 0; num < cant_thread; num++) {
+        newThread[num] = new Thread(name[num]);
+        newThread[num]->Fork(SimpleThread, (void *) name[num]);
+    }
+
+    SimpleThread((void *) "1st");
+}
