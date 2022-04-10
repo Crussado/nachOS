8672655603e19fccf9eea6ffc5fbd0bca21edc63
Julio <julioolivencia891@gmail.com>
2022-04-10 01:24:12 -0300

    se agrega test join

---
+++ code/threads/thread_test_join.cc
@@ -0,0 +1,40 @@
+#include "thread_test_prod_cons.hh"
+#include "system.hh"
+#include "../lib/debug.hh"
+#include "../lib/assert.hh"
+#include <stdio.h>
+
+
+static const int CANT_HIJOS = 10;
+static const int TIEMPO = 100;
+
+
+static void
+Hijo(void *n_) {
+    unsigned *n = (unsigned *) n_;
+    for (unsigned i = 0; i < TIEMPO; i++) {
+        currentThread->Yield();
+    }
+    printf("Hijo %u finished.\n", *n);
+}
+
+void
+ThreadTestJoin()
+{
+    Thread *t[CANT_HIJOS];
+    for (unsigned i = 0; i < CANT_HIJOS; i++) {
+        printf("Launching padre %u.\n", i);
+        char *name = new char [16];
+        sprintf(name, "padre %u", i);
+        unsigned *n = new unsigned;
+        *n = i;
+        t[i] = new Thread(name);
+        t[i]->Fork(Hijo, (void *) n);
+    }
+
+    for (unsigned i = 0; i < CANT_HIJOS; i++) {
+        t[i]->Join();
+    }
+
+    printf("El padre se va a dormir.\n");
+}
